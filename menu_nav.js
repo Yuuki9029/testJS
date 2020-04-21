60e9a2e84c653017dee22615d30c650faa2cbe91
Yuuki9029 <51696669+Yuuki9029@users.noreply.github.com>
2020-04-21 16:56:27 +0700

    add Menu Nav

---
+++ menu_nav.js
@@ -0,0 +1,18 @@
+// get menu list
+
+var menu = document.querySelectorAll('#dropdown > li');
+
+for (var i = 0; i < menu.length; i++){
+    menu[i].addEventListener("click", function()
+    {
+        // hidden sub menu
+        var menuList = document.querySelectorAll('#dropdown > li > ul');
+        for ( var j = 0 ; j < menu.List.length; j++){
+            menuList[j].style.display = "none";
+        }
+
+        // show menu
+        this.children[1].style.display = "block";
+    });
+
+}
